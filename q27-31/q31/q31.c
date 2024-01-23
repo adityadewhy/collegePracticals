0db023d0195a789b25575684385e81a8729be6c7
aditya <adityadewhy@gmail.com>
2024-01-23 10:16:58 +0530

    b done

--- q27-31/q31/q31.c
+++ q27-31/q31/q31.c
@@ -7,12 +7,35 @@ ABCD
 
 #include <stdio.h>
 
+// void main(){
+//     int temp = 0;
+//     for (int i = 65 ; i <= 70; i++){
+//         for(int j = 65; j <= i ; j++){
+//             printf("%c",j);
+//         }
+//         printf("\n");        
+//     }
+// }
+
+/*
+   *
+  **
+ ***
+****
+*/
+
 void main(){
-    int temp = 0;
-    for (int i = 65 ; i <= 70; i++){
-        for(int j = 65; j <= i ; j++){
-            printf("%c",j);
+    int n = 0;
+    printf("enter no of lines : ");
+    scanf("%d",&n);
+
+    for(int i = 1; i<=n; i++){
+        for(int space = n; space>=i; space--){
+            printf(" ");
         }
-        printf("\n");        
+        for(int j = 1; j <= i; j++){
+            printf("*");
+        }
+        printf("\n");
     }
 }
