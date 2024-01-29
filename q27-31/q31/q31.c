f91516c5b7f34b2c7c5e6a4388174ac5064d7c12
aditya <adityadewhy@gmail.com>
2024-01-29 13:48:59 +0530

    q31 done

--- q27-31/q31/q31.c
+++ q27-31/q31/q31.c
@@ -1,11 +1,11 @@
+#include <stdio.h>
+
 /*
 A
 AB
 ABC
 ABCD
 */
-
-#include <stdio.h>
 
 // void main(){
 //     int temp = 0;
@@ -24,18 +24,65 @@ ABCD
 ****
 */
 
-void main(){
-    int n = 0;
-    printf("enter no of lines : ");
-    scanf("%d",&n);
+// void main(){
+//     int n = 0;
+//     printf("enter no of lines : ");
+//     scanf("%d",&n);
 
+//     for(int i = 1; i<=n; i++){
+//         for(int space = n; space>=i; space--){
+//             printf(" ");
+//         }
+//         for(int j = 1; j <= i; j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
+
+/*
+4321
+ 321
+  21
+   1
+*/
+// void main(){
+//     int n;
+//     printf("till what number : ");
+//     scanf("%d",&n);
+//     for(int i = 1 ; i<= n; i++){
+//         for(int space = 0; space<i ; space++){
+//             printf(" ");
+//         }
+//         for(int num = n-i+1; num>0 ; num--){
+//             printf("%d", num);
+//         }
+        
+//         printf("\n");
+//     }
+    
+// }
+
+//    A
+//   ABA
+//  ABCBA
+// ABCDCBA
+
+void main(){
+    int n;
+    printf("how many lines : ");
+    scanf("%d",&n);
     for(int i = 1; i<=n; i++){
-        for(int space = n; space>=i; space--){
+        for(int space = n-i; space>0; space--){
             printf(" ");
         }
-        for(int j = 1; j <= i; j++){
-            printf("*");
+        for(int ascLetter = 0; ascLetter<i; ascLetter++){
+            printf("%c",ascLetter+65);
+        }
+        for(int desLetter = i-2; desLetter>=0; desLetter--){
+            printf("%c",desLetter+65);
         }
         printf("\n");
     }
+
 }
--- q27-31/q31/tempCodeRunnerFile.c
+++ q27-31/q31/tempCodeRunnerFile.c
@@ -1,1 +1,15 @@
+void main(){
+    int n = 0;
+    printf("enter no of lines : ");
+    scanf("%d",&n);
+
+    for(int i = 1; i<=n; i++){
+        for(int space = n; space>=i; space--){
+            printf(" ");
+        }
+        for(int j = 1; j <= i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
 }
